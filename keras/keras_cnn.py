a249f70bf3e7193d70fa32d9497a04d7af0590d6
Michael McCarver <mmccarver27@gmail.com>
2020-11-17 21:49:22 -0500

    adding cnn-8 model

---
+++ keras/keras_cnn.py
@@ -0,0 +1,160 @@
+"""
+https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-from-scratch-for-mnist-handwritten-digit-classification/
+"""
+
+# baseline cnn model for mnist
+from keras.datasets import mnist
+from keras.layers import Conv2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import MaxPooling2D
+from keras.models import load_model
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+from matplotlib import pyplot
+from numpy import append
+from numpy import argmax
+from numpy import mean
+from numpy import std
+import pandas as pd
+from sklearn.model_selection import KFold
+
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+
+    # print(testY[1])
+    # print(argmax(testY[1]))
+
+    return trainX, trainY, testX, testY
+
+
+def load_from_csv():
+    test_data = pd.read_csv("../data/test.csv")
+    testX = test_data.to_numpy()
+    # (trainX, trainY), (testX, testY) = mnist.load_data()
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    testX_norm = testX / 255.0
+    return testX_norm
+
+
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
+
+
+# define cnn model
+def define_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(10, activation='softmax'))
+    # compile model
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# evaluate a model using k-fold cross-validation
+def evaluate_model(dataX, dataY, n_folds=5):
+    scores, histories = list(), list()
+    # prepare cross validation
+    kfold = KFold(n_folds, shuffle=True, random_state=1)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(dataX):
+        # define model
+        model = define_model()
+        # select rows for train and test
+        trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
+        # fit model
+        history = model.fit(trainX, trainY, epochs=10, batch_size=32, validation_data=(testX, testY), verbose=0)
+        # evaluate model
+        _, acc = model.evaluate(testX, testY, verbose=0)
+        print('> %.3f' % (acc * 100.0))
+        # stores scores
+        scores.append(acc)
+        histories.append(history)
+    # model.save("keras_custom_cnn.h5")
+    return scores, histories, model
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(histories):
+    for i in range(len(histories)):
+        # plot loss
+        pyplot.subplot(2, 1, 1)
+        pyplot.title('Cross Entropy Loss')
+        pyplot.plot(histories[i].history['loss'], color='blue', label='train')
+        pyplot.plot(histories[i].history['val_loss'], color='orange', label='test')
+        # plot accuracy
+        pyplot.subplot(2, 1, 2)
+        pyplot.title('Classification Accuracy')
+        pyplot.plot(histories[i].history['accuracy'], color='blue', label='train')
+        pyplot.plot(histories[i].history['val_accuracy'], color='orange', label='test')
+    pyplot.show()
+
+
+# summarize model performance
+def summarize_performance(scores):
+    # print summary
+    print('Accuracy: mean=%.3f std=%.3f, n=%d' % (mean(scores)*100, std(scores)*100, len(scores)))
+    # box and whisker plots of results
+    pyplot.boxplot(scores)
+    pyplot.show()
+
+
+def print_as_csv(pred):
+    # labels = [argmax(x) for x in pred]
+    pred_df = pd.DataFrame(pred, columns=["Label"])
+    # pred_df = pd.DataFrame(labels, columns=["Label"])
+    pred_df["ImageId"] = range(1, 28001)
+    pred_df.to_csv(r'keras_cnn_pred.csv')
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+    # load kaggle dataset
+    eval_data = load_from_csv()
+    model = load_model("keras_custom_cnn.h5")
+
+    # load dataset
+    # trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    # trainX, testX = prep_pixels(trainX, testX)
+    # evaluate model
+    # scores, histories, model = evaluate_model(trainX, trainY)
+    # learning curves
+    # summarize_diagnostics(histories)
+    # summarize estimated performance
+    # summarize_performance(scores)
+
+    # pred = model.predict(eval_data)
+    pred = model.predict_classes(eval_data)
+    # print(pred.shape)
+    print_as_csv(pred)
+    # print(pred.shape)
+    
+
+# entry point, run the test harness
+run_test_harness()
+# load_from_csv()
